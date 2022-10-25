6f5d15456915823f524ad20d7ca2c2a842f4ca2e
Haizea <haize@DESKTOP-8QPK9LI>
2022-10-25 08:49:16 +0200

    New: Model (subject to change)

---
+++ src/datatransferobject/Model.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package datatransferobject;
+
+/**
+ *
+ * @author haize
+ */
+public interface Model {
+    public Package doSignIn(User user);
+    public Package doSignUp(User user); 
+}
