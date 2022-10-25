f94cdd0b773e74fbc9622dd378943da7f2e9a898
Haizea <haize@DESKTOP-8QPK9LI>
2022-10-25 10:07:11 +0200

    Modified: (Model) javadoc.

--- src/datatransferobject/Model.java
+++ src/datatransferobject/Model.java
@@ -1,13 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package datatransferobject;
 
 /**
- *
  * @author haize
+ * Interface of the model. It specifies the behavior of the model implementations.
  */
 public interface Model {
     public Package doSignIn(User user);
