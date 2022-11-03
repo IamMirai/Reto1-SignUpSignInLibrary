1c7939633e01a0f9ac1e68f26e92ba0f934c970f
2dam <2dam@TartangaLH.eus>
2022-11-03 13:43:07 +0100

    fixed model interface

--- src/datatransferobject/Model.java
+++ src/datatransferobject/Model.java
@@ -1,10 +1,16 @@
 package datatransferobject;
 
+import exceptions.ConnectionErrorException;
+import exceptions.InvalidUserException;
+import exceptions.MaxConnectionExceededException;
+import exceptions.TimeOutException;
+import exceptions.UserExistException;
+
 /**
  * @author haize
  * Interface of the model. It specifies the behavior of the model implementations.
  */
 public interface Model {
-    public User doSignIn(User user);
-    public User doSignUp(User user); 
+    public User doSignIn(User user) throws InvalidUserException, ConnectionErrorException, TimeOutException, MaxConnectionExceededException;
+    public User doSignUp(User user) throws UserExistException, ConnectionErrorException, TimeOutException, MaxConnectionExceededException; 
 }
